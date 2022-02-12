74ea5facc8c2bc10ce065c3961566dd6d9650aa0
Harshil-Walia <99045708+Harshil-Walia@users.noreply.github.com>
2022-02-11 21:28:04 -0500

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,66 @@
+# import socket module
+from socket import *
+# In order to terminate the program
+import sys
+
+
+def webServer(port=5500):
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    #Prepare a server socket
+    serverSocket.bind(('127.0.0.1', port))
+    
+    #Fill in start
+    serverSocket.listen()
+    #Fill in end
+
+    while True:
+        #Establish the connection
+        print('Ready to serve...')
+        print("Hi")
+        connectionSocket, addr = serverSocket.accept()
+        print(f"Accepted connection request from {connectionSocket}:{addr}")
+        #Fill in start      #Fill in end
+        
+
+        try:
+
+            try:
+                message = serverSocket.recv(1024)
+                #Fill in start    #Fill in end
+                filename = message.split()[1]
+                f = open(filename[1:])
+                outputdata = message
+                #Fill in start     #Fill in end
+                
+                #Send one HTTP header line into socket.
+                #Fill in start
+                connectionSocket.send('<h1>Header File</h1>')
+                #Fill in end
+
+                #Send the content of the requested file to the client
+                for i in range(0, len(outputdata)):
+                    connectionSocket.send(outputdata[i].encode())
+
+                connectionSocket.send("\r\n".encode())
+                connectionSocket.close()
+
+            except IOError:
+            # Send response message for file not found (404)
+            #Fill in start
+                print('File not found! (Error 404)')
+            #Fill in end
+
+
+            #Close client socket
+            #Fill in start
+                serverSocket.close()
+            #Fill in end
+
+        except (ConnectionResetError, BrokenPipeError):
+            pass
+
+        serverSocket.close()
+        sys.exit()  # Terminate the program after sending the corresponding data
+
+if __name__ == "__main__":
+    webServer(5500)
